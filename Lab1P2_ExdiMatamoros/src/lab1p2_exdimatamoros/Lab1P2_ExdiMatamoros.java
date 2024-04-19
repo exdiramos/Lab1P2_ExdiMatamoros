89a06d76e108eb9227e03eefaefc389407e2fcc1
exdiramos <exdimatamoros@unitec.edu>
2024-04-19 16:11:36 -0600

    Committt2

--- Lab1P2_ExdiMatamoros/src/lab1p2_exdimatamoros/Lab1P2_ExdiMatamoros.java
+++ Lab1P2_ExdiMatamoros/src/lab1p2_exdimatamoros/Lab1P2_ExdiMatamoros.java
@@ -15,30 +15,49 @@ import java.util.ArrayList;
 public class Lab1P2_ExdiMatamoros {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter a odd number greater than 4 for the matrix size:");
-        int size = scanner.nextInt();
+        System.out.print("Ingrese un numero mayor a 4 para el tamaño de la matriz: ");
+        int tamaño = scanner.nextInt();
 
-        // Ensure the size is an odd number greater than 4
-        while (size <= 4 || size % 2 == 0) {
-            System.out.println("The size should be an odd number greater than 4, try again:");
-            size = scanner.nextInt();
+        // Tamaño de la matrizi sea un numero mayor a 4
+        while (tamaño <= 4 || tamaño % 2 == 0) {
+            System.out.println("El tamaño de la matriz debe de ser un numero mayor a 4:");
+            tamaño = scanner.nextInt();
         }
 
-        // Create a matrix of specified size with random numbers
-        int[][] matrix = new int[size][size];
-        Random random = new Random();
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                matrix[i][j] = random.nextInt(10);
+        // Generar una matriz con numero randoms
+        int[][] matriz = new int[tamaño][tamaño];
+        Random numerito = new Random();
+        for (int i = 0; i < tamaño; i++) {
+            for (int j = 0; j < tamaño; j++) {
+                matriz[i][j] = numerito.nextInt(10);
             }
         }
 
-        // Print the matrix
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                System.out.print(matrix[i][j] + " ");
+        // Imprimir Matriz
+        for (int i = 0; i < tamaño; i++) {
+            for (int j = 0; j < tamaño; j++) {
+                System.out.print(matriz[i][j] + " ");
             }
             System.out.println();
         }
+    }
+     public static void bubbleSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int i = bubbleSortUtil(arr, l, r);
+            bubbleSort(arr, l, i - 1);
+            bubbleSort(arr, i + 1, r);
+        }
+    }
+
+    public static int bubbleSortUtil(int[] arr, int l, int r) {
+        int i, j, temp;
+        for (i = l, j = l + 1; j <= r; i++, j++) {
+            if (arr[i] > arr[j]) {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        return i - 1;
     }
 }//Fin clase.
